05525afa512f1c12a2384b4077baa45562b1ed36
Roman <rroman2115@yandex.ru>
2026-03-29 22:03:27 +0300

    07 lesson

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const { register, login, getUsers, deleteUser, updateUser, getRoles } = require("./controllers/user");
+const {
+    register,
+    login,
+    getUsers,
+    deleteUser,
+    updateUser,
+    getRoles,
+} = require("./controllers/user");
+const {
+    addPost,
+    editPost,
+    deletePost,
+    getPosts,
+    getPost,
+} = require("./controllers/post");
 const mapUser = require("./helpers/mapUser");
 const authenticated = require("./middleware/authenticated");
 const hasRole = require("./middleware/hasRole");
-const ROLES = require("./constants/roles")
+const ROLES = require("./constants/roles");
+const mapPost = require("./helpers/mapPost");
 const PORT = 3001;
 const app = express();
 
@@ -14,9 +29,14 @@ app.use(express.json());
 
 app.post("/register", async (req, res) => {
     try {
-        const { user, token } = await register(req.body.login, req.body.password);
-        res.cookie("token", token, { httpOnly: true })
-            .send({ error: null, user: mapUser(user) });
+        const { user, token } = await register(
+            req.body.login,
+            req.body.password,
+        );
+        res.cookie("token", token, { httpOnly: true }).send({
+            error: null,
+            user: mapUser(user),
+        });
     } catch (e) {
         res.send({ error: e.message || "Unknown error" });
     }
@@ -39,33 +59,73 @@ app.post("/logout", async (req, res) => {
     res.cookie("token", "", { httpOnly: true }).send({});
 });
 
+app.get("/posts", async (req, res) => {
+    const { posts, lastPage } = await getPosts(req.query.search, req.query.limit, req.query.page)
 
-app.use(authenticated)
+
+    res.send({ data: {lastPage, posts: posts.map(mapPost)} })
+})
+
+app.get("/posts/:id", async (req, res) => {
+    const post = await getPost(req.params.id)
+
+    res.send({ data: mapPost(post) })
+})
+
+
+
+app.use(authenticated);
+
+app.post("/posts", hasRole([ROLES.ADMIN]), async (req, res) => {
+    const newPost = await addPost({
+        title: req.body.title, 
+        image: req.body.image, 
+        content: req.body.content, 
+    })
+    res.send({ data: mapPost(newPost) })
+})
+
+app.patch("/posts/:id", hasRole([ROLES.ADMIN]), async (req, res) => {
+    const updatedPost = await editPost(req.params.id, {
+        title: req.body.title, 
+        image: req.body.image, 
+        content: req.body.content, 
+    })
+
+    res.send({ data: mapPost(updatedPost) })
+})
+
+
+app.delete("/posts/:id", hasRole([ROLES.ADMIN]), async (req, res) => {
+    await deletePost(req.params.id)
+
+    res.send({error: null})
+})
 
 app.get("/users", hasRole([ROLES.ADMIN]), async (req, res) => {
-    const users = await getUsers()
-
-    res.send({ data: users })
-})
+    const users = await getUsers();
+    
+    res.send({ data: users });
+});
 
 app.get("/users/roles", hasRole([ROLES.ADMIN]), async (req, res) => {
-    const roles = await getRoles()
-
-    res.send({ data: roles })
-})
+    const roles = await getRoles();
+    
+    res.send({ data: roles });
+});
 
 app.patch("/users/:id", hasRole([ROLES.ADMIN]), async (req, res) => {
     const newUser = await updateUser(req.params.id, {
-        role: req.body.roleId
-    })
-
-    res.send({data: mapUser(newUser)})
-})
+        role: req.body.roleId,
+    });
+    
+    res.send({ data: mapUser(newUser) });
+});
 
 app.delete("/users/:id", hasRole([ROLES.ADMIN]), async (req, res) => {
-    await deleteUser(req.params.id)
-    res.send({ error: null })
-})
+    await deleteUser(req.params.id);
+    res.send({ error: null });
+});
 
 mongoose.connect("mongodb://user:mongopass@localhost:27017/").then(() => {
     app.listen(PORT, () => {
--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,5 +1,47 @@
+const Post = require("../models/Post")
+
+
 // add post
+function addPost(post) {
+    return Post.create(post)
+}
+// edit post
+async function editPost(id, post) {
+    const newPost = await Post.findByIdAndUpdate(id, post, { retirnDocument: "after" })
 
+    return newPost
+}
 // delete post 
+function deletePost(id) {
+    return Post.deleteOne({_id: id})
+}
 
-// read post
+// get posts (pagination)
+async function getPosts(search="", limit=10, page=1) {
+    const [posts, count] = await Promise.all([
+        Post.find({title: {$regex: search, $options: "i"}}) // i - регистронеависимый
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .sort({createdAt: -1}),
+        Post.countDocuments({title: {$regex: search, $options: "i"}})
+    ])
+
+    return {
+        posts, 
+        lastPage: Math.ceil(count / limit)
+    }
+}
+
+// get post
+function getPost(id) {
+    return Post.findById(id)
+}
+
+module.exports = {
+    addPost,
+    editPost,
+    deletePost,
+    getPosts,
+    getPost,
+    addPost
+}
